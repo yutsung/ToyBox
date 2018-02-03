9f44d19e873827985023893157ea5c9cebf7628f
Yu-Tsung Chen <eoo585246@gmail.com>
2018-02-03 16:26:19 +0800

    print formatted modify

--- learn_decorator/demo3.py
+++ learn_decorator/demo3.py
@@ -3,13 +3,19 @@
 from functools import wraps
 import time
 
+
 def count_runtime(func):
     @wraps(func)
     def with_runtime(*args, **kwargs):
         starttime = time.time()
         out_func = func(*args, **kwargs)
         endtime = time.time()
-        print("runtime: %10.8f" % (endtime-starttime))
+        print(
+            '{}, runtime: {:10.8f}'.format(
+                func.__name__, 
+                endtime - starttime
+            )
+        )
         return out_func
     return with_runtime
 
