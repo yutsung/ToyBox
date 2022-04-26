65a3e7e98ef7d5455bbb4cb34bfaa11c98d32531
yutsung <eoo585246@gmail.com>
2022-04-26 11:37:28 +0800

    multiprocessing, map -> starmap

--- learn_multiprocessing/mtp_pool_test02.py
+++ learn_multiprocessing/mtp_pool_test02.py
@@ -3,15 +3,14 @@
 import numpy as np
 import multiprocessing as mtp
  
-def multiply(param):
-    x1, x2 = param
+def multiply(x1, x2):
     return x1 * x2
  
 def main():
-    x1 = np.arange(20)
-    x2 = np.arange(20)
-    p = mtp.Pool(3)
-    p_out = p.map(multiply, zip(x1, x2))
+    x1 = np.arange(5)
+    x2 = np.array([1, 2, 3, 0, 9])
+    p = mtp.Pool(2)
+    p_out = p.starmap(multiply, zip(x1, x2))
     print(p_out)
  
 if __name__ == "__main__":
